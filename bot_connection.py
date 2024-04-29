42f0eb8c6a6a339ffc094260365661343bafbb4f
Angel Ceron Ramos <86043771+angxdes@users.noreply.github.com>
2024-04-29 02:56:41 -0600

    Add files via upload

---
+++ bot_connection.py
@@ -0,0 +1,45 @@
+from telegram import Update
+from telegram.ext import (Application,
+                          CommandHandler,
+                          MessageHandler,
+                          filters,
+                          ContextTypes)
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+# ESto sirve para crear comandos, este por ejemplo es "start"
+async def start(update: Update,
+                context: ContextTypes):
+    await update.message.reply_text("Hola, soy un mensaje de prueba\
+                                    estoy aqui para ayudarte")
+
+# La siguiente funcion servira para responder mensajes con elifs
+
+def answer(text: str):
+    text = text.lower()
+    if "hola" in text:
+        return "Hola, como estas cachorrita?"
+    elif "adios" in text:
+        return "No te vayas cachorrita"
+
+# Funcion para responder al usuario dependiendo el
+# mensaje encontrado en la lista de arriba
+
+async def chat_message(update: Update,
+                       context: ContextTypes.DEFAULT_TYPE) -> None:
+    type = update.message.chat.type
+    if type == "private":
+        text = update.message.text
+        response = answer(text)
+        await update.message.reply_text(response)
+
+
+if __name__ == "__main__":
+    app = Application.builder().token("6332076176:AAFtght4HB-88N5SYpu3kJ6HQvzMhcmRI9o").build()
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(MessageHandler(filters.TEXT, chat_message))
+    app.run_polling(poll_interval = 1)
+  
+
---
+++ var_entorno.py
@@ -0,0 +1,7 @@
+import os
+
+# Define tu variable de Python
+mi_variable = "6332076176:AAFtght4HB-88N5SYpu3kJ6HQvzMhcmRI9o"
+
+# Establece la variable de entorno
+os.environ['TOKEN'] = mi_variable
